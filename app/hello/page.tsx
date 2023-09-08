c13c4cf158ba7b7e35398a4cd8212d6afc36e045
Nick McCollum <mccollumn@webtrends.com>
2023-09-08 00:44:58 +0000

    Change to client page

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,9 +1,22 @@
-const Page = async () => {
-  const result = await fetch(`${process.env.API_ENDPOINT}/api/hello`);
-  console.log("Result:", result);
-  const data = await result.json();
-  console.log("Data:", data);
-  return <div>{data.message}</div>;
+"use client";
+
+import React from "react";
+
+const Page = () => {
+  const [data, setData] = React.useState<any>();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const result = await fetch(`/api/hello`);
+      console.log("Result:", result);
+      const data = await result?.json();
+      console.log("Data:", data);
+      setData(data);
+    };
+    getData();
+  }, []);
+
+  return <div>{data?.message}</div>;
 };
 
 export default Page;
