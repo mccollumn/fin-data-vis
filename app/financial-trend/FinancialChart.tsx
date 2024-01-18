1c22bb2d704b9fe0f2c5a01a8799be02f3a8a8cf
Nick McCollum <mccollumn@webtrends.com>
2024-01-17 18:05:15 -0800

    Create chart data

--- app/financial-trend/FinancialChart.tsx
+++ app/financial-trend/FinancialChart.tsx
@@ -1,15 +1,47 @@
 "use client";
 
 import { LineChart } from "@mui/x-charts";
+import React from "react";
 
-export const FinancialChart = ({ data }: any) => {
-  console.log("Chart data:", data);
-  if (data.length === 0) return null;
+export const FinancialChart = ({ data, selectedKeys }: any) => {
+  console.log("FinancialChart data:", data);
+  const chartData: any = React.useMemo(
+    () => getChartData(data, selectedKeys),
+    [data, selectedKeys]
+  );
+  if (!data || !data.length) return null;
+  console.log("Chart data:", chartData);
   return (
     <LineChart
-      dataset={data}
-      xAxis={[{ dataKey: "fiscalYear" }]}
-      series={[{ dataKey: "value" }]}
+      dataset={chartData}
+      xAxis={[{ dataKey: "year" }]}
+      series={selectedKeys.map((selected: any) => {
+        return {
+          dataKey: selected.value,
+          label: selected.label,
+        };
+      })}
     />
   );
+};
+
+const getChartData = (data: any, selectedKeys: any) => {
+  if (!data) return [];
+  const tranformData = data
+    .filter((financial: any) =>
+      selectedKeys.some((selected: any) => selected.value === financial.key)
+    )
+    .filter((financial: any) => financial.fiscalPeriod === "FY")
+    .reduce((prev: any, current: any) => {
+      const currentRecord = prev[current.fiscalYear] || {};
+      return {
+        ...prev,
+        [current.fiscalYear]: {
+          ...currentRecord,
+          year: current.fiscalYear,
+          [current.key]: current.value,
+        },
+      };
+    }, {});
+  return Object.values(tranformData);
 };
